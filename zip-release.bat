dbe52dcd0d055f0ebacd88cf43dd9b6604bf0cbb
EleonoreMizo <fuck@fuck.fuck>
2022-04-11 17:51:05 +0200

    zip: added the test files + minor misc stuff

--- zip-release.bat
+++ zip-release.bat
@@ -10,6 +10,7 @@
 @mkdir "reltmp\build\unix\m4"
 @mkdir "reltmp\build\win\common"
 @mkdir "reltmp\build\win\fmtconv"
+@mkdir "reltmp\build\win\test"
 @mkdir "reltmp\win32"
 @mkdir "reltmp\win64"
 @xcopy /I "doc"                                        "reltmp\doc"
@@ -21,6 +22,7 @@
 @xcopy /I "src\fmtcavs"                                "reltmp\src\fmtcavs"
 @xcopy /I "src\fmtcl"                                  "reltmp\src\fmtcl"
 @xcopy /I "src\fstb"                                   "reltmp\src\fstb"
+@xcopy /I "src\test"                                   "reltmp\src\test"
 @xcopy /I "src\vsutl"                                  "reltmp\src\vsutl"
 @copy     "src\*.cpp"                                  "reltmp\src"
 @copy     "src\*.h"                                    "reltmp\src"
@@ -39,14 +41,16 @@
 @copy     "build\win\fmtconv\*.vcxproj.filters"        "reltmp\build\win\fmtconv"
 @copy     "build\win\fmtconv\ReleaseWin32\fmtconv.dll" "reltmp\win32"
 @copy     "build\win\fmtconv\Releasex64\fmtconv.dll"   "reltmp\win64"
+@copy     "build\win\test\*.vcxproj"                   "reltmp\build\win\test"
+@copy     "build\win\test\*.vcxproj.filters"           "reltmp\build\win\test"
 @copy     "*.md"                                       "reltmp"
 @copy     "COPYING"                                    "reltmp"
 
 @cd reltmp
 
-del /S *.lo *.o *.dirstamp
+del /S *.lo *.o *.dirstamp *.deps
 
-@echo fmtconv - Format conversion tools for Vapoursynth and Avisynth+. | "C:\Program Files (x86)\Infozip\zip.exe" -r -o -9 -z "..\releases\fmtconv-rnew.zip" "*.*"
+@echo fmtconv - Format conversion tools for Vapoursynth and Avisynth+ | "C:\Program Files (x86)\Infozip\zip.exe" -r -o -9 -z "..\releases\fmtconv-rnew.zip" "*.*"
 @cd ..
 
 @rmdir /s /q "reltmp"
